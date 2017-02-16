bf1de9fbe05288b16aac454c73f0848434438c5b
Afonso Bordado <afonsobordado@az8.co>
2017-02-16 12:53:00 +0000

    Remove feature proc_macro

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#![feature(proc_macro)]
 #![doc(test(attr(allow(unused_variables), deny(warnings))))]
 
 extern crate hyper;

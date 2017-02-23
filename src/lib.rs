e2572914de0b724928b65dde9bf547afb15dc121
Afonso Bordado <afonsobordado@az8.co>
2017-02-23 11:30:54 +0000

    Use feature flag only on nightly

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#![feature(test)]
+#![cfg_attr(feature = "nightly", feature(test))]
+
 #![doc(test(attr(allow(unused_variables), deny(warnings))))]
 
 extern crate hyper;
